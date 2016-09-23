57b9fc62670509a215e6359892d6dfbbab52453d
Adam <adamschunk@gmail.com>
2016-09-23 00:23:19 -0400

    accidently omitted this file in previous commit

--- get_draft_order.py
+++ get_draft_order.py
@@ -1,13 +1,15 @@
+import csv
+import numpy as np
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-#for year in range(2000,2016):
 driver = webdriver.Firefox()
-driver.get("http://www.drafthistory.com/index.php/years/" + str(2000))
+for year in range(2000,2017):
+	driver.get("http://www.drafthistory.com/index.php/years/" + str(year))
+	csvFile = open("draft_order_"+str(year)+".csv", 'wb')
 
-for row in driver.find_elements_by_tag_name("tr")[2:4]:
-	for value in row.find_elements_by_tag_name("td"):
-		print value.text
-
-
-print(element[5].get_attribute("value"))
+	for row in driver.find_elements_by_tag_name("tr")[2:-9]:
+		values = [str(x.text) for x in row.find_elements_by_tag_name("td")]
+		print(values)
+		writer = csv.writer(csvFile)
+		writer.writerow(values)
